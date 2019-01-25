a600f5f62d3cfe81b760b1a83acf9ff1c065ed02
cfcforever <chelseaforeverchampion@gmail.com>
2019-01-25 22:00:05 +0100

    Update 00_anomaly.R

--- SWG/00_anomaly.R
+++ SWG/00_anomaly.R
@@ -1,62 +1,10 @@
 library(ncdf4)
 library(fields)
-#
-
-#### load msl ####
-nc <- nc_open("/Volumes/Data-ExFAT/DATA/nc/NA/msl_1979-01-01to2018-07-31_NA_1.5x1.5.nc")
-data1 = ncvar_get(nc, "msl"); range(data1)
-LON = ncvar_get(nc, "longitude")
-LAT = ncvar_get(nc, "latitude")
-time = ncvar_get(nc, "time"); length(time); length(time)/4
-nc_close(nc) 
-
-nt = length(time)
-data = (data1[,,seq(from=1,to=nt,by = 4)] + data1[,,seq(from=2,to=nt,by = 4)] + data1[,,seq(from=3,to=nt,by = 4)] + data1[,,seq(from=4,to=nt,by = 4)])/4
-dim(data)
-range(data)
-
-DATE = atoms(timeSequence(from="1979-01-01",to="2018-07-31",by='day'))
-
-save(data, LON, LAT, DATE, file = "~/Documents/LSCE/SWG/NA_1.5/msl_1979-01-01to2018-07-31_NA_1.5x1.5_daily.RData")
-
-#### load z500 ####
-nc <- nc_open("/Volumes/Data-ExFAT/DATA/nc/NA/z500_1979-01-01to2018-07-31_NA_1.5x1.5.nc")
-data1 = ncvar_get(nc, "z"); range(data1)
-LON = ncvar_get(nc, "longitude")
-LAT = ncvar_get(nc, "latitude")
-time = ncvar_get(nc, "time"); length(time); length(time)/4
-nc_close(nc) 
-
-nt = length(time)
-data = (data1[,,seq(from=1,to=nt,by = 4)] + data1[,,seq(from=2,to=nt,by = 4)] + data1[,,seq(from=3,to=nt,by = 4)] + data1[,,seq(from=4,to=nt,by = 4)])/4
-dim(data)
-range(data)
-
-DATE = atoms(timeSequence(from="1979-01-01",to="2018-07-31",by='day'))
-
-save(data, LON, LAT, DATE, file = "~/Documents/LSCE/SWG/NA_1.5/z500_1979-01-01to2018-07-31_NA_1.5x1.5_daily.RData")
-
-#### load sst ####
-# nc <- nc_open("/Volumes/Data-ExFAT/DATA/nc/NA/sst_1979-01-01to2018-07-31_NA_1.5x1.5.nc")
-nc <- nc_open("/Volumes/Data-ExFAT/DATA/nc/NA/sst_1979-01-01to2018-07-31_NA_1.5x1.5_small.nc")
-data1 = ncvar_get(nc, "sst"); range(data1)
-LON = ncvar_get(nc, "longitude")
-LAT = ncvar_get(nc, "latitude")
-time = ncvar_get(nc, "time"); length(time); length(time)/4
-nc_close(nc) 
-
-nt = length(time)
-data = (data1[,,seq(from=1,to=nt,by = 4)] + data1[,,seq(from=2,to=nt,by = 4)] + data1[,,seq(from=3,to=nt,by = 4)] + data1[,,seq(from=4,to=nt,by = 4)])/4
-dim(data)
-range(data)
-
-DATE = atoms(timeSequence(from="1979-01-01",to="2018-07-31",by='day'))
-
-# save(data, LON, LAT, DATE, file = "~/Documents/LSCE/SWG/NA_1.5/sst_1979-01-01to2018-07-31_NA_1.5x1.5_daily.RData")
-save(data, LON, LAT, DATE, file = "~/Documents/LSCE/SWG/NA_1.5/sst_1979-01-01to2018-07-31_NA_1.5x1.5_daily_small.RData")
 
 
-#### Create anomaly.RData ####
+# Create anomaly.RData ----------------------------------------------------
+
+
 var = "msl"; # var = "sst"
 
 input.file = paste0("~/Documents/LSCE/SWG/NA_1.5/", var, "_1979-01-01to2018-07-31_NA_1.5x1.5_daily.RData")
