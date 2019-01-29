36cf218d33421906ba7b52bb89f619ba7630c37c
cfcforever <chelseaforeverchampion@gmail.com>
2019-01-29 19:37:05 +0100

    Update

--- RUN_code.R
+++ RUN_code.R
@@ -1,5 +1,33 @@
+#### Conditional SWG for temperature in different cities ---------------------
 city.names = c("Paris", "Madrid", "Stockholm")
-for (city in city.names[2:3]){
+for (city in city.names){
   source("conditional_SWG_temperature.R") 
   rm(list=ls(all=TRUE))
 }
+####
+
+
+
+#### FAR ---------------------------------------------------------------------
+
+####
+
+
+
+#### Changes of intensity ----------------------------------------------------
+city.names = c("Paris", "Madrid", "Stockholm")
+for (city in city.names){
+  source("changes_of_intensity.R") 
+  rm(list=ls(all=TRUE))
+}
+####
+
+
+
+#### Anomaly maps ------------------------------------------------------------
+city.names = c("Paris", "Madrid", "Stockholm")
+for (city in city.names){
+  source("anomaly_maps.R") 
+  rm(list=ls(all=TRUE))
+}
+####
